39be05478ac0fbb6b4018f2b2f81912af5e6cb3f
Eric Hamilton <ericjean873@gmail.com>
2022-12-06 22:57:54 -0500

    re-wrote StartView to accomodate for different requirements.

--- cogs/pokemon.py
+++ cogs/pokemon.py
@@ -5,42 +5,33 @@ from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 from discord.ext import commands
 from discord import app_commands
-from typing import Dict, Any, List, Optional, Union
+from typing import Dict, Any, List, Literal, Optional, Union
 from uuid import UUID, uuid4
 
 
-class TimeDeltaTransformer(app_commands.Transformer):
-    @classmethod
-    async def transform(
-        cls, interaction: discord.Interaction, argument: int
-    ) -> timedelta:
-        delta = timedelta(hours=int(argument))
-        return delta
+# @dataclass
+# class VoteData:
+#     id: UUID
+#     votes: List[int]
+#     end_time: datetime
+#     dex_no: str
 
+#     @classmethod
+#     def _finish(cls):
+#         cls.tourney: TourneyData
+#         del cls._finish
 
-@dataclass
-class VoteData:
-    id: UUID
-    votes: List[int]
-    end_time: datetime
-    dex_no: str
-
-    @classmethod
-    def _finish(cls):
-        cls.tourney: TourneyData
-        del cls._finish
-
-    async def create_in_store(self, db: Union[asyncpg.Connection, asyncpg.Pool]):
-        await db.execute(
-            """
-            INSERT INTO pokemon.vote VALUES ($1, $2, $3, $4, $5);
-        """,
-            self.id,
-            self.votes,
-            self.end_time,
-            self.dex_no,
-            self.tourney.id,
-        )
+#     async def create_in_store(self, db: Union[asyncpg.Connection, asyncpg.Pool]):
+#         await db.execute(
+#             """
+#             INSERT INTO pokemon.vote VALUES ($1, $2, $3, $4, $5);
+#         """,
+#             self.id,
+#             self.votes,
+#             self.end_time,
+#             self.dex_no,
+#             self.tourney.id,
+#         )
 
 
 @dataclass
@@ -48,42 +39,57 @@ class TourneyData:
     id: UUID
     channel: Union[int, discord.TextChannel, discord.VoiceChannel]
     guild: Union[int, discord.Guild]
-    gen_order: List[str]
-    interval: timedelta
-    active_votes: List[VoteData]
-    _generations = [
-        "generation-i",
-        "generation-ii",
-        "generation-iii",
-        "generation-iv",
-        "generation-v",
-        "generation-vi",
-        "generation-vii",
-        "generation-viii",
-        "generation-ix",
-    ]
+    interval: timedelta = timedelta(24)
+    start_time: datetime = datetime.now()
+    current_gen: int = 1
+    vote_on_gen: bool = False
+    active: bool = False
 
-    @classmethod
-    def available_options(cls, used_vals=[]) -> List[discord.SelectOption]:
-        options = []
-        for val in cls._generations:
-            if val not in used_vals:
-                label = val.replace("-", " ").replace("g", "G")
-                label = label[0:10] + label[10:].upper()
-                options.append(discord.SelectOption(label=label, value=val))
-        return options
+    # @classmethod
+    # def available_options(cls, used_vals=[]) -> List[discord.SelectOption]:
+    #     options = []
+    #     for val in cls._generations:
+    #         if val not in used_vals:
+    #             label = val.replace("-", " ").replace("g", "G")
+    #             label = label[0:10] + label[10:].upper()
+    #             options.append(discord.SelectOption(label=label, value=val))
+    #     return options
 
-    async def create_in_store(self, db: Union[asyncpg.Connection, asyncpg.Pool]):
-        await db.execute(
-            """
-            INSERT INTO pokemon.tourney VALUES ($1, $2, $3, $4, $5);
-        """,
-            self.id,
-            self.channel.id,
-            self.guild.id,
-            self.gen_order,
-            self.interval,
-        )
+    # async def create_in_store(self, db: Union[asyncpg.Connection, asyncpg.Pool]):
+    #     await db.execute(
+    #         """
+    #         INSERT INTO pokemon.tourney VALUES ($1, $2, $3, $4, $5);
+    #     """,
+    #         self.id,
+    #         self.channel.id,
+    #         self.guild.id,
+    #         self.gen_order,
+    #         self.interval,
+    #     )
+
+
+@dataclass
+class CombatantData:
+    id: UUID
+    end_time: datetime
+    dex_no: int
+    tourney: Union[UUID, TourneyData]
+    message: Union[int, discord.Message, discord.PartialMessage, None] = None
+
+
+class VoteData:
+    id: UUID
+    user: Union[int, discord.User]
+    rank: str
+    tourney: Union[UUID, TourneyData]
+    combatant: Union[UUID, CombatantData]
+
+
+class Tourney:
+    def __init__(self, data: TourneyData, db: Union[asyncpg.Connection, asyncpg.Pool]):
+        self.data = data
+        self.active_pokemon = List[CombatantData]
+        self.db = db
 
     async def current_votes(self, db: Union[asyncpg.Connection, asyncpg.Pool]) -> None:
         """This method is called when the cog has been reloaded, and the `active_votes` attribute is empty. It pulls any
@@ -92,7 +98,7 @@ class TourneyData:
         Args:
             db (Union[asyncpg.Connection, asyncpg.Pool]): The database that this method pulls from.
         """
-        if len(self.active_votes) != 0:
+        if len(self.active_pokemon) != 0:
             return
         records = await db.fetch(
             """
@@ -113,9 +119,9 @@ class TourneyData:
                     dex_no=r.get("dex_no"),
                     tourney=self.id,
                 )
-                self.active_votes.append(vote)
+                self.active_pokemon.append(vote)
 
-    async def next_votes(self, db: Union[asyncpg.Connection, asyncpg.Pool]):
+    async def next_pokemon(self, db: Union[asyncpg.Connection, asyncpg.Pool]):
         """This method progresses to the next set of Pokemon in the progression of the generation order,
         and changes the `active_votes` attribute. If active votes is empty, it populates it with the initial votes.
 
@@ -124,11 +130,12 @@ class TourneyData:
         """
         client = ClientSession()
         next_pkmn = None
-        current_gen = self.gen_order[0]
         # called when starting a new tourney.
-        if len(self.active_votes) == 0:
+        if len(self.active_pokemon) == 0:
             gen_response = await (
-                await client.get(f"https://pokeapi.co/api/v2/generation/{current_gen}/")
+                await client.get(
+                    f"https://pokeapi.co/api/v2/generation/{self.data.current_gen}/"
+                )
             ).json()
             # getting the first pokemon of the generation.
             next_pkmn = await (
@@ -136,11 +143,11 @@ class TourneyData:
             ).json()
         else:
             next_pkmn = await (
-                await client.get(self.active_votes[len(self.active_votes) - 1])
+                await client.get(self.active_pokemon[len(self.active_pokemon) - 1])
             ).json()
             if next_pkmn["generation"]["name"] != current_gen:
                 current_gen = next_pkmn["generation"]["name"]
-            self.active_votes.clear()
+            self.active_pokemon.clear()
         dex_no = next_pkmn["id"]
         prev_names = []
         # We have at least 10 active votes per session.
@@ -149,11 +156,11 @@ class TourneyData:
             vote = VoteData(
                 id=uuid4(),
                 votes=[0, 0, 0, 0, 0, 0],
-                end_time=datetime.now() + self.interval,
+                end_time=datetime.now() + self.data.interval,
                 dex_no=dex_no,
             )
             vote.tourney = self
-            self.active_votes.append(vote)
+            self.active_pokemon.append(vote)
             # advancing to the next mon
             dex_no += 1
             prev_names.append(next_pkmn["name"])
@@ -164,124 +171,104 @@ class TourneyData:
             evolves_from = next_pkmn["evolves_from_species"]
             if not (
                 (evolves_from != None and evolves_from["name"] in prev_names)
-                or len(self.active_votes) < 10
+                or len(self.active_pokemon) < 10
             ):
                 break
-        for vote in self.active_votes:
+        for vote in self.active_pokemon:
             await vote.create_in_store(db)
         await client.close()
 
 
-VoteData._finish()
-
-
-class TourneyStartView(discord.ui.View):
-    def __init__(self, *, tourney: TourneyData, bot: commands.Bot):
+class StartView(discord.ui.View):
+    def __init__(self, *, bot: commands.Bot, t_data: TourneyData):
         super().__init__(timeout=None)
-        self.tourney = tourney
         self.bot = bot
-        self.initial_votes: List[VoteData] = []
-
-    @discord.ui.select(
-        placeholder="Pick a Generation!", options=TourneyData.available_options()
-    )
-    async def gen_select(self, inter: discord.Interaction, select: discord.ui.Select):
-        self.tourney.gen_order.append(select.values[0])
-        if len(self.tourney.gen_order) < 9:
-            select.options = TourneyData.available_options(self.tourney.gen_order)
-        else:
-            select.disabled = True
-        self.remove_gen_button.disabled = False
-        if len(self.tourney.gen_order) == 9:
-            self.start_button.disabled = False
-        else:
-            self.start_button.disabled = True
-        await inter.response.edit_message(embed=self.embed, view=self)
-
-    @discord.ui.button(label="Remove Last Gen", disabled=True, emoji="➖")
-    async def remove_gen_button(
-        self, inter: discord.Interaction, btn: discord.ui.Button
-    ):
-        self.tourney.gen_order.pop()
-        self.gen_select.disabled = False
-        self.start_button.disabled = True
-        self.gen_select.options = TourneyData.available_options(self.tourney.gen_order)
-        await inter.response.edit_message(embed=self.embed, view=self)
-        if len(self.tourney.gen_order) == 0:
-            btn.disabled = True
-            self.start_button.disabled = False
+        self.t_data = t_data
+        self.tourney = None
 
     @discord.ui.button(label="Start Tourney", disabled=False, emoji="▶️")
     async def start_button(self, inter: discord.Interaction, btn: discord.ui.Button):
-        if len(self.tourney.gen_order) == 0:
-            for gen in TourneyData._generations:
-                self.tourney.gen_order.append(gen)
-        await self.tourney.create_in_store(self.bot.db)
-        await self.tourney.next_votes(self.bot.db)
-        await inter.response.edit_message(
-            content="Tourney successfully generated!", embed=None, view=None
-        )
+        self.t_data.active = True
+        self.t_data.start_time = datetime.now()
+        self.tourney = Tourney(self.t_data, self.bot.db)
+        await inter.response.edit_message("Tourney has been successfully generated!")
 
     @property
     def embed(self):
         embed = discord.Embed(
             title="Start Pokemon Tourney",
-            description="This is an interactive menu for starting a Pokemon tourney and setting the order of generations for the Tourney. If no generations are selected, it defaults to 1-9.",
+            description="Confirm that the below settings are correct:",
         )
-        for idx, gen in enumerate(self.tourney.gen_order):
-            label = gen.replace("-", " ").replace("g", "G")
-            label = label[0:10] + label[10:].upper()
-            embed.add_field(name=idx + 1, value=label, inline=False)
+        embed.add_field(
+            name="Tourney Channel", value=self.t_data.channel.mention, inline=True
+        )
+        embed.add_field(
+            name="Vote on Next Generation",
+            value=f"`{self.t_data.vote_on_gen}`",
+            inline=True,
+        )
+        embed.add_field(
+            name="Time Interval Between Votes",
+            value=f"{int(self.t_data.interval.total_seconds()) // 3600} hours",
+            inline=False,
+        )
         return embed
 
-    async def start(self, interaction: discord.Interaction):
-        self.interaction = interaction
-        await interaction.response.send_message(
-            embed=self.embed, view=self, ephemeral=True
-        )
-        self.message = await interaction.original_response()
+    async def start(self, inter: discord.Interaction):
+        await inter.response.send_message(embed=self.embed, view=self, ephemeral=True)
 
 
 class Pokemon(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.db: Union[asyncpg.Connection, asyncpg.Pool] = bot.db
-        self.active_tourneys: List[TourneyData] = []
+        self.active_tourneys: List[Tourney] = []
 
     async def load_tourneys(self):
-        records = await self.db.fetch(
-            """
-            SELECT t.* FROM pokemon.tourney t;
-        """
-        )
+        records = await self.db.fetch("""select * from pokemon_tourney.vote;""")
         for r in records:
-            tourney = TourneyData(
+            t_data = TourneyData(
                 id=r.get("id"),
-                channel=discord.Object(r.get("channel")),
-                guild=discord.Object(r.get("guild")),
-                gen_order=r.get("gen_order"),
+                channel=discord.Object(r.get("channel_id")),
+                guild=discord.Object(r.get("guild_id")),
                 interval=r.get("time_interval"),
-                active_votes=[],
+                start_time=r.get("start_time"),
+                current_gen=r.get("current_gen"),
+                auto_gen=r.get("auto_gen"),
+                active=r.get("active"),
             )
-            await tourney.current_votes(self.db)
+            tourney = Tourney(t_data, self.db)
             self.active_tourneys.append(tourney)
 
     async def init_db(self):
         query = """
-            CREATE SCHEMA IF NOT EXISTS pokemon;
-            CREATE TABLE IF NOT EXISTS pokemon.tourney(
-                id UUID PRIMARY KEY,
-                channel BIGINT,
-                guild_id BIGINT,
-                gen_order TEXT ARRAY,
-                time_interval INTERVAL
+            create schema if not exists pokemon_tourney;
+            create table if not exists pokemon_tourney.data
+            (
+            	id            uuid                                   not null primary key,
+            	channel_id    bigint                                 not null,
+            	guild_id      bigint                                 not null,
+            	time_interval interval                               not null,
+            	start_time    timestamp with time zone default now() not null,
+            	current_gen   integer                  default 1     not null,
+            	vote_on_gen   boolean                  default false not null,
+            	active        boolean                  default false not null
             );
-            CREATE TABLE IF NOT EXISTS pokemon.vote(
-                id UUID PRIMARY KEY,
-                votes INT ARRAY,
-                end_time TIMESTAMP WITH TIME ZONE,
-                dex_no INT,
-                tourney_id UUID REFERENCES pokemon.tourney (id) ON DELETE CASCADE
+            create table if not exists pokemon_tourney.combatant
+            (
+            	id         uuid                     not null primary key,
+            	end_time   timestamp with time zone not null,
+            	dex_no     integer                  not null,
+            	message_id bigint,
+            	tourney_id uuid                     not null references pokemon_tourney.data on delete cascade
+            );
+            create table if not exists pokemon_tourney.vote
+            (
+            	id           integer not null primary key,
+            	user_id      bigint  not null,
+            	rank         char    not null,
+            	tourney_id   uuid    not null references pokemon_tourney.data on delete cascade,
+            	combatant_id uuid    not null references pokemon_tourney.combatant on delete cascade
             );
         """
         await self.db.execute(query)
@@ -294,48 +281,40 @@ class Pokemon(commands.Cog):
         name="pokemon", description="Commands for ranking Pokemon."
     )
 
-    @pokemon.command(name="start")
+    @pokemon.command(
+        name="start", description="Starts a new tourney for ranking Pokemon."
+    )
+    @app_commands.describe(
+        channel="The channel for sending tourney messages in. Should be isolated from other channels.",
+        vote_on_gen="Whether or not to vote on generations. Defaults to false, where it will run from Gen 1 up.",
+        interval="The frequency by which to run votes. Defaults to 24 hours.",
+    )
     @app_commands.checks.has_permissions(administrator=True)
     async def pokemon_start(
         self,
-        interaction: discord.Interaction,
+        inter: discord.Interaction,
         channel: Union[discord.TextChannel, discord.VoiceChannel],
-        interval: app_commands.Transform[
-            Optional[timedelta], TimeDeltaTransformer
-        ] = None,
+        vote_on_gen: Literal["True", "False"] = "False",
+        interval: app_commands.Range[int, 1, 48] = 24,
     ):
-        """Generates a new tourney for ranking Pokemon. Admin-only command.
-
-        Args:
-            interaction (discord.Interaction): The discord interaction for this app command.
-            channel (discord.TextChannel): The channel to send the tourney messages in.
-            interval (app_commands.Transform[ Optional[timedelta], TimeDeltaTransformer ], optional): The length of
-            time, in hours, between batches of votes. Defaults to 24 if not provided.
-        """
         # First checking if there is an active tourney in this guild. If there is, we must throw an exception.
         records = await self.db.fetch(
-            """
-            SELECT * FROM pokemon.tourney WHERE guild = $1;
-        """,
-            interaction.guild.id,
+            """select from pokemon_tourney.data t where t.guild_id = $1;""",
+            inter.guild.id,
         )
         if len(records) != 0:
             raise app_commands.AppCommandError(
                 "Tourney already started in guild, stop tourney to begin a new one."
             )
-        if interval == None:
-            interval = timedelta(hours=24)
-
-        tourney_data = TourneyData(
+        t_data = TourneyData(
             id=uuid4(),
             channel=channel,
-            guild=interaction.guild,
-            interval=interval,
-            gen_order=[],
-            active_votes=[],
+            guild=inter.guild,
+            interval=timedelta(hours=interval),
+            vote_on_gen=True if vote_on_gen == "True" else False,
         )
-        start_view = TourneyStartView(bot=self.bot, tourney=tourney_data)
-        await start_view.start(interaction=interaction)
+        start_view = StartView(bot=self.bot, t_data=t_data)
+        await start_view.start(inter)
         await start_view.wait()
         self.active_tourneys.append(start_view.tourney)
 
